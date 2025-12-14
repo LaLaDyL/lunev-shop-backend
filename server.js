e43d7b624d696a12fe54816728279767e455af6a
KATANA <lunevandrey2006@mail.ru>
2025-12-14 19:42:42 +0300

    Удален фильтр is_active, так как колонки нет

--- server.js
+++ server.js
@@ -179,7 +179,6 @@ app.get('/api/products', async (req, res) => {
       SELECT p.*, c.name as category_name 
       FROM products p 
       LEFT JOIN categories c ON p.category_id = c.category_id 
-      WHERE p.is_active = TRUE 
       ORDER BY p.product_id
     `);
     
